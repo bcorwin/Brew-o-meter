0eaf3e89ce43f188064ceb400c87b16cdd82b707
Bcorwin <bscorwin@gmail.com>
2015-03-19 17:11:49 -0500

    Added chk_sum value to Arduino code
    Simply the sum of the values it is sending. If the sum of the received
    values do not match the chk_sum, throw it out.

--- Brew_o_meter/Brew_o_meter.ino
+++ Brew_o_meter/Brew_o_meter.ino
@@ -8,10 +8,14 @@ void setup() {
 void loop() {
   int light_val = analogRead(light);
   float temp_amb_val = 1.8*(analogRead(temp_amb)*4960.0/1024.0 - 500.0)/10.0 + 32.0;
+  float chk_sum = light_val + temp_amb;
 
   Serial.print("{");
   
-  Serial.print("'light_amb':");
+  Serial.print("'chk_sum':");
+  Serial.print(chk_sum);
+  
+  Serial.print(", 'light_amb':");
   Serial.print(light_val);
   
   Serial.print(", 'temp_amb':");
